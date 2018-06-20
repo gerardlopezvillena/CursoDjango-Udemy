247c2b38bab5253c65aafb965c5018e8caf88ef0
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-06-20 09:26:54 +0200

    Fi models

--- webpersonal/portafoli/models.py
+++ webpersonal/portafoli/models.py
@@ -8,9 +8,9 @@ class Project(models.Model):
     # que el que et genera es una taula a la base de dades.
     # Les taules tenen files i columnes, i cada atribut
     # daquesta classes sera uha xooumna diferent
-    title= models.CharField()
+    title= models.CharField(max_length=200)
     # cada atribut es una columna (aixo esta definit aixi a models.Model
     description=models.TextField()
-    image=ImageField()
-    created=DateTimeField(auto_now_add=True)
-    updated=DateTimeField(auto_now=True)
+    image=models.ImageField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
