a82c618beb792c12cd5ff920d00cc40a80ca2071
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-06-22 09:09:53 +0200

    Config. Panell Administrador 2

--- webpersonal/portafoli/apps.py
+++ webpersonal/portafoli/apps.py
@@ -3,3 +3,20 @@ from django.apps import AppConfig
 
 class PortafoliConfig(AppConfig):
     name = 'portafoli'
+    verbose_name = 'Portafoli' 
+    # latribut 'name' no es pot
+    # canviar ja que va lligat
+    # a la aplicacio, pero si volem
+    # canviar el nom que surt al
+    # panell dadministracio de
+    # django, ho podem fer
+    # utilitzant latribut
+    # verbose_name. A mes hem
+    # de modificar larxiu settings
+    # de la carpeta principal
+    # (en aquest cas: webpersonal)
+    # i el que posarem es:
+    # 'portfoli.apps.PortafolioConfig'
+    # en definitiva, estic cridant
+    # la classe daquest fitxer
+
--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'portafoli',
+    'portafoli.apps.PortafoliConfig',
 ]
 
 MIDDLEWARE = [
