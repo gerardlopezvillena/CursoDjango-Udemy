e74395a33c0a3ad8b3877ca30e5ddc18dc426965
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-06-26 20:26:20 +0200

    config admin 4

--- webpersonal/portafoli/admin.py
+++ webpersonal/portafoli/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import  Project
 
 # Register your models here.
-admin.site.register(Project)
+
+# Configurem el panell de controo per a que
+# es vegin la data de creacio i la data de
+# modificacio del projecte
+class ProjectAdmin(admin.ModelAdmin):
+    # Puc posar el nom que vulgui pero sembla ser
+    # que hi ha consens en aquest tipus de nom
+    readonly_fields=('created', 'updated')
+
+admin.site.register(Project, ProjectAdmin)
+# he registrat els dos models Project i ProjectAdmin
