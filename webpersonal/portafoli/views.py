7b8d4642f135680b0f3538ce03c9ca5d3b390840
Gerard López Villena <gerardlopezvillena@gmail.com>
2018-06-29 09:02:59 +0200

    MVT 2

--- webpersonal/portafoli/views.py
+++ webpersonal/portafoli/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def portafoli(request):
-    return(render(request,'portafoli/portafoli.html'))
+    projects=Project.objects.all()
+    # mesta creant una llista anomenada projects que inclou
+    # tots els objectes creats per el model Project (que es
+    # una classe)
+    return(render(request,'portafoli/portafoli.html', {'projects':projects}))
+    # puc afegir totes les varibles que vulgui a la vista definint
+    # una clau al diccionari i posant com a valor de la clau, la
+    # variable dessitjada. A aquest diccionari sel coneix com a
+    # diccionari de context
 
